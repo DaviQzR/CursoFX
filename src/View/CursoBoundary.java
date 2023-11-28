1f0649771943fd7fae02943e2d38935b1ceba01a
DaviQzR <125469425+DaviQzR@users.noreply.github.com>
2023-11-28 03:09:10 -0300

    Adicionando o Table View

--- src/View/CursoBoundary.java
+++ src/View/CursoBoundary.java
@@ -2,12 +2,15 @@ package View;
 
 import Model.CursoEntity;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.collections.FXCollections;
 
 public class CursoBoundary extends Application {
 
@@ -21,6 +24,11 @@ public class CursoBoundary extends Application {
 
     private Button btnAdicionar = new Button("Adicionar");
     private Button btnPesquisar = new Button("Pesquisar");
+    private Button btnAtualizar = new Button("Atualizar");
+    private Button btnExcluir = new Button("Excluir");
+    private Button btnLimpar = new Button("Limpar Seleção");
+
+    private TableView<CursoEntity> tabelaCursos = new TableView<>();
 
     public static void main(String[] args) {
         launch(args);
@@ -30,50 +38,79 @@ public class CursoBoundary extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("CRUD de Cursos");
 
-        Pane root = new Pane();
+        VBox root = new VBox(10);
 
         criarCamposTexto(root);
         criarBotoes(root);
+        criarTabela(root);
 
-        Scene scene = new Scene(root, 400, 300);
+        Scene scene = new Scene(root, 800, 500);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    private void criarCamposTexto(Pane root) {
-        adicionarLabelETextField("ID:", txtId, 20, 20, 200, 20, root);
-        adicionarLabelETextField("Nome:", txtNome, 20, 60, 200, 60, root);
-        adicionarLabelETextField("Código do Curso:", txtCodigo, 20, 100, 200, 100, root);
-        adicionarLabelETextField("Nome do Coordenador:", txtCoordenador, 20, 140, 200, 140, root);
-        adicionarLabelETextField("Quantidade de Alunos:", txtQuantidade, 20, 180, 200, 180, root);
+    private void criarCamposTexto(VBox root) {
+        adicionarLabelETextField("ID:", txtId, root);
+        adicionarLabelETextField("Nome:", txtNome, root);
+        adicionarLabelETextField("Código do Curso:", txtCodigo, root);
+        adicionarLabelETextField("Nome do Coordenador:", txtCoordenador, root);
+        adicionarLabelETextField("Quantidade de Alunos:", txtQuantidade, root);
     }
 
-    private void adicionarLabelETextField(String labelText, TextField textField, double labelX, double labelY, double fieldX, double fieldY, Pane root) {
+    private void adicionarLabelETextField(String labelText, TextField textField, VBox root) {
+        HBox hbox = new HBox(10);
         Label label = new Label(labelText);
-
-        label.setLayoutX(labelX);
-        label.setLayoutY(labelY);
-        textField.setLayoutX(fieldX);
-        textField.setLayoutY(fieldY);
-
-        root.getChildren().addAll(label, textField);
+        hbox.getChildren().addAll(label, textField);
+        root.getChildren().add(hbox);
     }
 
-    private void criarBotoes(Pane root) {
-        btnAdicionar.setLayoutX(20);
-        btnAdicionar.setLayoutY(220);
+    private void criarBotoes(VBox root) {
+        HBox hbox = new HBox(10);
 
-        btnPesquisar.setLayoutX(200);
-        btnPesquisar.setLayoutY(220);
-
-        configurarBotoes();
-
-        root.getChildren().addAll(btnAdicionar, btnPesquisar);
-    }
-
-    private void configurarBotoes() {
         btnAdicionar.setOnAction(event -> adicionarCurso());
         btnPesquisar.setOnAction(event -> pesquisarCurso());
+        btnAtualizar.setOnAction(event -> atualizarCurso());
+        btnExcluir.setOnAction(event -> excluirCurso());
+        btnLimpar.setOnAction(event -> limparSelecao());
+
+        hbox.getChildren().addAll(btnAdicionar, btnPesquisar, btnAtualizar, btnExcluir, btnLimpar);
+        root.getChildren().add(hbox);
+    }
+
+    private void criarTabela(VBox root) {
+        TableColumn<CursoEntity, Integer> colId = new TableColumn<>("ID");
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+
+        TableColumn<CursoEntity, String> colNome = new TableColumn<>("Nome");
+        colNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
+
+        TableColumn<CursoEntity, Integer> colCodigo = new TableColumn<>("Código do Curso");
+        colCodigo.setCellValueFactory(new PropertyValueFactory<>("codCurso"));
+
+        TableColumn<CursoEntity, String> colCoordenador = new TableColumn<>("Coordenador");
+        colCoordenador.setCellValueFactory(new PropertyValueFactory<>("coordenador"));
+
+        TableColumn<CursoEntity, Integer> colQuantidade = new TableColumn<>("Quantidade de Alunos");
+        colQuantidade.setCellValueFactory(new PropertyValueFactory<>("qtdAlunos"));
+
+        tabelaCursos.getColumns().addAll(colId, colNome, colCodigo, colCoordenador, colQuantidade);
+        tabelaCursos.setItems(FXCollections.observableArrayList(cursoControl.getCursos()));
+
+        tabelaCursos.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+            if (newSelection != null) {
+                preencherCampos(newSelection);
+            }
+        });
+
+        root.getChildren().add(tabelaCursos);
+    }
+
+    private void preencherCampos(CursoEntity curso) {
+        txtId.setText(String.valueOf(curso.getId()));
+        txtNome.setText(curso.getNome());
+        txtCodigo.setText(String.valueOf(curso.getCodCurso()));
+        txtCoordenador.setText(curso.getCoordenador());
+        txtQuantidade.setText(String.valueOf(curso.getQtdAlunos()));
     }
 
     private void adicionarCurso() {
@@ -88,7 +125,6 @@ public class CursoBoundary extends Application {
 
             limparCampos();
         } catch (NumberFormatException e) {
-            // Trate o erro de conversão de número aqui
             e.printStackTrace();
         }
     }
@@ -99,22 +135,46 @@ public class CursoBoundary extends Application {
             CursoEntity cursoEncontrado = cursoControl.pesquisarCursoEntity(idPesquisa);
 
             if (cursoEncontrado != null) {
-                atualizarCamposTexto(cursoEncontrado);
+                preencherCampos(cursoEncontrado);
             } else {
                 limparCampos();
             }
         } catch (NumberFormatException e) {
-            // Trate o erro de conversão de número aqui
             e.printStackTrace();
         }
     }
 
-    private void atualizarCamposTexto(CursoEntity curso) {
-        txtId.setText(String.valueOf(curso.getId()));
-        txtNome.setText(curso.getNome());
-        txtCodigo.setText(String.valueOf(curso.getCodCurso()));
-        txtCoordenador.setText(curso.getCoordenador());
-        txtQuantidade.setText(String.valueOf(curso.getQtdAlunos()));
+    private void atualizarCurso() {
+        try {
+            int id = Integer.parseInt(txtId.getText());
+            String nome = txtNome.getText();
+            int codCurso = Integer.parseInt(txtCodigo.getText());
+            String coordenador = txtCoordenador.getText();
+            int qtdAlunos = Integer.parseInt(txtQuantidade.getText());
+
+            CursoEntity cursoAtualizado = new CursoEntity(id, nome, codCurso, coordenador, qtdAlunos);
+            cursoControl.atualizarCurso(cursoAtualizado);
+
+            limparCampos();
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void excluirCurso() {
+        try {
+            int id = Integer.parseInt(txtId.getText());
+            cursoControl.excluirCurso(id);
+
+            limparCampos();
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void limparSelecao() {
+        tabelaCursos.getSelectionModel().clearSelection();
+        limparCampos();
     }
 
     private void limparCampos() {
--- src/View/CursoControl.java
+++ src/View/CursoControl.java
@@ -15,7 +15,6 @@ public class CursoControl {
     private static final String SENHA = "123456";
     private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=curso;user=" + USUARIO + ";password=" + SENHA + ";encrypt=false;";
 
-
     public CursoControl() {
         // Carregar cursos do banco de dados na inicialização, se necessário
         carregarCursosDoBanco();
@@ -39,32 +38,49 @@ public class CursoControl {
         return curso;
     }
 
+    public void atualizarCurso(CursoEntity cursoAtualizado) {
+        // Atualizar na lista local
+        cursos.removeIf(c -> c.getId() == cursoAtualizado.getId());
+        cursos.add(cursoAtualizado);
+
+        // Atualizar no banco de dados
+        atualizarCursoNoBanco(cursoAtualizado);
+    }
+
+    public void excluirCurso(int id) {
+        // Excluir da lista local
+        cursos.removeIf(c -> c.getId() == id);
+
+        // Excluir do banco de dados
+        excluirCursoNoBanco(id);
+    }
+
     public List<CursoEntity> getCursos() {
         return cursos;
     }
 
     private void carregarCursosDoBanco() {
-      // Lógica para carregar cursos do banco de dados para a lista local
-      try (Connection connection = DriverManager.getConnection(URL, USUARIO, SENHA)) {
-          String sql = "SELECT id, nome, codCurso, coordenador, qtdAlunos FROM Curso";
-          try (PreparedStatement statement = connection.prepareStatement(sql)) {
-              try (ResultSet resultSet = statement.executeQuery()) {
-                  while (resultSet.next()) {
-                      int id = resultSet.getInt("id");
-                      String nome = resultSet.getString("nome");
-                      int codCurso = resultSet.getInt("codCurso");
-                      String coordenador = resultSet.getString("coordenador");
-                      int qtdAlunos = resultSet.getInt("qtdAlunos");
+        // Lógica para carregar cursos do banco de dados para a lista local
+        try (Connection connection = DriverManager.getConnection(URL, USUARIO, SENHA)) {
+            String sql = "SELECT id, nome, codCurso, coordenador, qtdAlunos FROM Curso";
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    while (resultSet.next()) {
+                        int id = resultSet.getInt("id");
+                        String nome = resultSet.getString("nome");
+                        int codCurso = resultSet.getInt("codCurso");
+                        String coordenador = resultSet.getString("coordenador");
+                        int qtdAlunos = resultSet.getInt("qtdAlunos");
 
-                      CursoEntity curso = new CursoEntity(id, nome, codCurso, coordenador, qtdAlunos);
-                      cursos.add(curso);
-                  }
-              }
-          }
-      } catch (SQLException e) {
-          e.printStackTrace(); 
-      }
-  }
+                        CursoEntity curso = new CursoEntity(id, nome, codCurso, coordenador, qtdAlunos);
+                        cursos.add(curso);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace(); 
+        }
+    }
 
     private void adicionarCursoNoBanco(CursoEntity curso) {
         // Lógica para adicionar curso ao banco de dados
@@ -83,9 +99,39 @@ public class CursoControl {
         }
     }
 
-    private CursoEntity buscarCursoNoBanco(int idPesquisa) {
-        
-        return null;
+    private void atualizarCursoNoBanco(CursoEntity curso) {
+        // Lógica para atualizar curso no banco de dados
+        try (Connection connection = DriverManager.getConnection(URL, USUARIO, SENHA)) {
+            String sql = "UPDATE Curso SET nome=?, codCurso=?, coordenador=?, qtdAlunos=? WHERE id=?";
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setString(1, curso.getNome());
+                statement.setInt(2, curso.getCodCurso());
+                statement.setString(3, curso.getCoordenador());
+                statement.setInt(4, curso.getQtdAlunos());
+                statement.setInt(5, curso.getId());
+                statement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace(); 
+        }
     }
 
+    private void excluirCursoNoBanco(int id) {
+        // Lógica para excluir curso do banco de dados
+        try (Connection connection = DriverManager.getConnection(URL, USUARIO, SENHA)) {
+            String sql = "DELETE FROM Curso WHERE id=?";
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setInt(1, id);
+                statement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace(); 
+        }
+    }
+
+    private CursoEntity buscarCursoNoBanco(int idPesquisa) {
+        // Lógica para buscar curso no banco de dados
+        // (Implemente de acordo com a sua necessidade)
+        return null;
+    }
 }
